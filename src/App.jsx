d4a4a648b1b8edaed078905310de8fa1b4588a03
Furkan Cingirit <cingirit.20@gmail.com>
2025-03-05 04:00:20 +0300

    Completed Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,27 @@
-import { Routes, Route} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "../src/pages/Home";
 import Trainings from "./pages/Trainings";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
+import "./css/app.css";
 
 function App() {
   return (
-    <>
-    <Navbar/>
-    <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/trainings" element={<Trainings />} />
-      </Routes>
-      <Footer/>
-    </>
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/trainings" element={<Trainings />} />
+          </Routes>
+          <Footer />
+        </div>
+      </div>
+    </div>
   );
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,83 @@
-import React from 'react'
+import { NavLink } from "react-router-dom";
+import { IoMdSearch } from "react-icons/io";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../css/navbar.css";
 
 function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <nav className="navbar navbar-expand-xl navbar-light container-fluid">
+      {/* TOGGLE BUTTON */}
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+      {/* LOGO */}
+      <NavLink to={"/"} className="navbar-brand ms-2">
+        <img
+          src="https://www.techcareer.net/assets/images/common/techcareer-logo.svg"
+          alt="TechCareer Logo"
+          className="logo-img"
+        />
+      </NavLink>
+
+      {/* SEARCH BAR*/}
+      <div className="input-group search-bar me-lg-3 mb-3 mb-lg-0 ms-3 me-3 d-none d-lg-flex">
+        <span
+          className="input-group-text bg-white border-end-0"
+          id="search-icon"
+        >
+          <IoMdSearch className="text-muted" />
+        </span>
+        <input
+          className="form-control border-start-0"
+          type="search"
+          placeholder="Etkinlik, İş İlanı, Blog Ara"
+          aria-label="Search"
+        />
+      </div>
+
+
+      {/* NAV LINKS */}
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <div className="d-flex flex-column flex-lg-row align-items-lg-center w-100">
+          {/* NAVLINKS */}
+          <div className="d-flex flex-column flex-lg-row gap-3">
+            <NavLink to={"/etkinlikler"} className="nav-link ms-3">
+              Etkinlikler
+            </NavLink>
+            <NavLink to={"/jobs"} className="nav-link ms-3">
+              İş İlanları
+            </NavLink>
+            <NavLink to={"/Trainings"} className="nav-link ms-3">
+              Eğitimler
+            </NavLink>
+            <NavLink to={"/tests"} className="nav-link ms-3">
+              Testler
+            </NavLink>
+            <NavLink to={"/cominity"} className="nav-link ms-3">
+              Kominite
+            </NavLink>
+          </div>
+        </div>
+      </div>
+
+      {/* LOGIN BUTTON AND SEARCH LOGO*/}
+      <div className="d-lg-none custom-search" >
+        <IoMdSearch/>
+      </div>
+      <button className="btn btn-primary border-xl-last">
+        Giriş / Üye Ol
+      </button>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
