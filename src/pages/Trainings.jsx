4e52c301bb6c3649b6e0a723d6762862c67317c6
Furkan Cingirit <cingirit.20@gmail.com>
2025-03-13 23:04:07 +0300

    Training Page Completed

--- src/pages/Trainings.jsx
+++ src/pages/Trainings.jsx
@@ -13,7 +13,7 @@ function Trainings() {
       title: "React Nedir?",
       subtitle: "React, UI oluşturmak için kullanılan bir kütüphanedir.",
       footertype: "type2",
-      content: ["bootcamp", "2"],
+      content: ["bootcamp", "FrameWork"],
       date: "10.02.2000",
     },
     {
@@ -142,7 +142,7 @@ function Trainings() {
             </div>
             {}
             {currentCards.map((card) => (
-              <div key={card.id} className="col-4">
+              <div key={card.id} className="col-lg-4 col-md-12 mb-4">
                 <Card
                   {...card}
                   Bottombtn={card.content.map((cont, i) => (
@@ -157,6 +157,125 @@ function Trainings() {
                 totalPages={totalPages}
                 onPageChange={setCurrentPage}
               ></Pagination>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="fdiv">
+        <div className="container">
+          <div className="row">
+            <div className="col-1">
+              <img src="/images/quizzes-icon.png" alt="" />
+            </div>
+            <div className="col-9">
+              <h3>Testlerimizi Gördünüz Mü?</h3>
+              <p className="fs-5" style={{ color: "rgb(135, 135, 135)" }}>
+                Teknoloji sektörüne özel hazırlanan ücretsiz teknoloji testleri
+                ile teknik yetkinlikleri ölçmeye anında başla.
+              </p>
+            </div>
+            <div className="col-2">
+              <button className="classic-button btn px-4 py-2">
+                Hemen Keşfet
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="fdiv">
+        <div className="container">
+          <div className="row">
+            <div
+              className="accordion accordion-flush border"
+              id="accordionFlushExample"
+            >
+              <div className="accordion-item">
+                <h2 className="accordion-header">
+                  <button
+                    className="accordion-button collapsed"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#flush-collapseOne"
+                    aria-expanded="false"
+                    aria-controls="flush-collapseOne"
+                  >
+                    <h4>Eğitimler Hakkında</h4>
+                  </button>
+                </h2>
+                <div
+                  id="flush-collapseOne"
+                  className="accordion-collapse collapse"
+                  data-bs-parent="#accordionFlushExample"
+                >
+                  <div className="accordion-body">
+                    <p className="my-4">
+                      Techcareer.net olarak, hem teknoloji paydaşları hem yeni
+                      başlayanlar hem de meraklıları için özel olarak
+                      tasarlanmış, geniş yelpazede, ücretsiz eğitim modülleri
+                      sunmaktayız. Amacımız, katılımcılara sektörde en çok talep
+                      gören bilgi ve yetkinlikleri kazandırarak, onların
+                      profesyonel dünyada başarılı bir kariyer inşa etmelerine
+                      katkı sağlamaktır. Eğitimlerimiz, alanında uzman ve
+                      deneyimli eğitmenler tarafından verilen kapsamlı
+                      içeriklerle desteklenmektedir.
+                    </p>
+                    <h3 className="my-4">
+                      Neden Techcareer.net Eğitim Modülleri?
+                    </h3>
+                    <p className="my-4">
+                      Techcareer.net’in eğitim modülleri, sadece teorik bilgi
+                      sunmakla kalmaz, aynı zamanda katılımcıların
+                      öğrendiklerini pratik uygulamalarla pekiştirmelerine
+                      olanak tanır. Her bir eğitim, başlangıç seviyesinden ileri
+                      düzeye kadar geniş bir skalada içerikler sunarak, farklı
+                      bilgi seviyelerindeki katılımcılar için uygun hale
+                      getirilmiştir. Bu sayede, teknoloji dünyasında yeni
+                      olanlar da, mevcut becerilerini bir üst seviyeye taşımak
+                      isteyenler de ihtiyaçlarına yönelik eğitim bulabilir.
+                    </p>
+                    <h3 className="my-4">Eğitim İçeriklerimiz</h3>
+                    <p className="my-4">
+                      Back-end'den front-end'e, DevOps'dan mobil uygulama
+                      geliştirmeye kadar pek çok farklı alanda, derin öğrenme
+                      sağlayacağınız ve edindiğiniz blgileri gerçek hayatta
+                      pratiğe dönüştürebileceğiniz pek çok eğitime ücretsiz bir
+                      şekilde techcareer.net'den erişebilirsiniz. Programlama
+                      dilleri arasında Java, Python ve JavaScript gibi dillerin
+                      yanı sıra, web geliştirme, veri bilimi, yapay zeka,
+                      yazılım mühendisliği ve siber güvenlik gibi çeşitli
+                      uzmanlık alanlarına yönelik eğitimler sunmaktayız.
+                    </p>
+                    <h3 className="my-4">Başarıya Giden Yolda Yanınızdayız</h3>
+                    <p className="my-4">
+                      Techcareer.net, eğitim modüllerini katılımcıların kariyer
+                      hedeflerine ulaşmalarına destek olmak amacıyla
+                      tasarlamıştır. Sunduğumuz eğitimlerle, sektörde rekabet
+                      avantajı elde edebilir, teknolojinin hızla gelişen
+                      dünyasında kendinizi güncel tutabilir ve kariyerinizde
+                      yeni zirvelere ulaşabilirsiniz. Eğitim programlarımız,
+                      sadece bilgi aktarmakla kalmaz, aynı zamanda
+                      katılımcıların kariyer yolculuklarında onlara rehberlik
+                      eder ve onları teknoloji sektörünün gelecekteki liderleri
+                      haline getirir.
+                    </p>
+                    <h3 className="my-4">
+                      Eğitimlere Katılmak İçin Hemen Kayıt Ol!
+                    </h3>
+                    <p className="my-4">
+                      Techcareer.net olarak, teknoloji ve yazılım alanında
+                      kariyer yapmayı hedefleyen katılımcılara, ihtiyaç
+                      duydukları bilgi ve becerileri kazandırmayı taahhüt
+                      ediyoruz. Eğitimlerimiz hakkında daha fazla bilgi almak ve
+                      kayıt olmak için lütfen eğitimler sayfamızı ziyaret edin.
+                      Techcareer.net ile teknoloji kariyerinizde sağlam
+                      adımlarla ilerleyin ve geleceğinizi inşa edin. Üstelik
+                      techcareer.net bünyesinde, uzaman eğitmenler tarafından
+                      hazırlanan tüm eğitimler hem sertifikalı hem de ücretsiz.
+                      Kayıt olup, hemen eğitimlere başlayabilirsin.{" "}
+                    </p>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
